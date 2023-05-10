57a738f3717a81b6170f9dd4f29fe63d6167e8ed
georgephilippas-ber@outlook.com <george.philippas@outlook.comgr>
2023-05-10 08:14:21 +0200

    Initial commit

--- providers/garminconnect-python/main.py
+++ providers/garminconnect-python/main.py
@@ -4,11 +4,14 @@ from garminconnect_client.garminconnect_client import GarminConnect
 from update.update import Update
 
 database_manager = DatabaseManager()
-garmin_connect = GarminConnect("george.philippas@outlook.com", "4AANmBuG3kyKT5r4")
-update_database = Update(database_manager, garmin_connect)
 
 
 def update(user_id: int):
+    # TODO: look up user_id and instantiate garmin_connect with the correct credentials
+
+    garmin_connect = GarminConnect("george.philippas@outlook.com", "4AANmBuG3kyKT5r4")
+    update_database = Update(database_manager, garmin_connect)
+
     if not garmin_connect.is_connected():
         garmin_connect.connect()
 
