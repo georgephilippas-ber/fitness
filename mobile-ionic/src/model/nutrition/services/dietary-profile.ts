6215f2073165a681c3be5169afc8a48fa09db0cd
George Philippas <george.philippas@outlook.com.gr>
2023-05-18 20:10:30 +0200

    Initial commit

--- mobile-ionic/src/model/nutrition/services/dietary-profile.ts
+++ mobile-ionic/src/model/nutrition/services/dietary-profile.ts
@@ -10,6 +10,7 @@ import {day_fromMillis, Period} from "@shared/common/features/time/period/period
 
 type product_consumption_pair_type = [product_consumption_type, product_type];
 
+
 export class DietaryProfile
 {
     constructor(private products: product_type[], private product_consumption: product_consumption_type[])
@@ -22,7 +23,7 @@ export class DietaryProfile
         return getPairs(this.product_consumption, this.products).filter(value => value[0].referenceDate >= period.getBeginning() && value[0].referenceDate <= period.getEnd());
     }
 
-    private pairs_byReferenceDate(referenceDate: number): [product_consumption_type, product_type][]
+    private pairs_dayByReferenceDate(referenceDate: number): [product_consumption_type, product_type][]
     {
         return this.pairs_byPeriod(new Period(day_fromMillis(referenceDate, "beginning").toMillis(), day_fromMillis(referenceDate, "end").toMillis()))
     }
@@ -38,5 +39,48 @@ export class DietaryProfile
         return pair[1].fundamental_nutrients[nutrient] / 100. * pair[0].quantity
     }
 
+    public servings(referenceDate: number)
+    {
+        return this.pairs_dayByReferenceDate(referenceDate).reduce((previousValue, currentValue) => previousValue + this.getServings_pair(currentValue), 0.);
+    }
 
+    public quantity(referenceDate: number)
+    {
+        return this.pairs_dayByReferenceDate(referenceDate).reduce((previousValue, currentValue) => previousValue + currentValue[0].quantity, 0.);
+    }
+
+    public energy(referenceDate: number)
+    {
+        return this.pairs_dayByReferenceDate(referenceDate).reduce((previousValue, currentValue) => previousValue + this.getNutrient_pair("energy", currentValue), 0.);
+    }
+
+    public protein(referenceDate: number)
+    {
+        return this.pairs_dayByReferenceDate(referenceDate).reduce((previousValue, currentValue) => previousValue + this.getNutrient_pair("protein", currentValue), 0.);
+    }
+
+    public carbohydrates(referenceDate: number)
+    {
+        return this.pairs_dayByReferenceDate(referenceDate).reduce((previousValue, currentValue) => previousValue + this.getNutrient_pair("carbohydrates", currentValue), 0.);
+    }
+
+    public fat(referenceDate: number)
+    {
+        return this.pairs_dayByReferenceDate(referenceDate).reduce((previousValue, currentValue) => previousValue + this.getNutrient_pair("fat", currentValue), 0.);
+    }
+
+    public fiber(referenceDate: number)
+    {
+        return this.pairs_dayByReferenceDate(referenceDate).reduce((previousValue, currentValue) => previousValue + this.getNutrient_pair("fiber", currentValue), 0.);
+    }
+
+    public sugar(referenceDate: number)
+    {
+        return this.pairs_dayByReferenceDate(referenceDate).reduce((previousValue, currentValue) => previousValue + this.getNutrient_pair("sugar", currentValue), 0.);
+    }
+
+    public sodium(referenceDate: number)
+    {
+        return this.pairs_dayByReferenceDate(referenceDate).reduce((previousValue, currentValue) => previousValue + this.getNutrient_pair("sodium", currentValue), 0.);
+    }
 }
