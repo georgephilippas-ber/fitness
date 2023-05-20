3e5088ff54bb374d7367d9897219752dcfc5fa86
George Philippas <george.philippas@outlook.com.gr>
2023-05-20 14:39:42 +0200

    Initial commit

--- mobile-ionic/src/components/modules/nutrition/atomic/product-consumption-journal-list/product-consumption-journal-list.tsx
+++ mobile-ionic/src/components/modules/nutrition/atomic/product-consumption-journal-list/product-consumption-journal-list.tsx
@@ -1,8 +1,10 @@
 import {product_consumption_type, product_type} from "@shared/common/schema/nutrition/nutrition";
-import {IonList} from "@ionic/react";
+import {IonIcon, IonList} from "@ionic/react";
 import {ProductConsumptionJournalEntry} from "../product-consumption-journal-entry/product-consumption-journal-entry";
 import {getPairs} from "./product-consumption-journal-list-controller";
 
+import "../product-consumption-journal-entry/product-consumption-journal-entry.css"
+import {alarm, fastFood} from "ionicons/icons";
 
 export function ProductConsumptionJournalList({product_consumption_array, products}: {
     product_consumption_array: product_consumption_type[],
@@ -10,8 +12,11 @@ export function ProductConsumptionJournalList({product_consumption_array, produc
 }) {
     return (
         <IonList>
-            <div>
-                calories
+            <div className={"product-consumption-journal-entry"}>
+                <div className={"servings"}><IonIcon icon={fastFood}/></div>
+                <div className={"name"}>Name</div>
+                <div className={"energy"}>Calories</div>
+                <div className={"time"}><IonIcon icon={alarm}/></div>
             </div>
             {getPairs(product_consumption_array, products).sort((a, b) => a[0].referenceDate - b[0].referenceDate).map(value => {
                 return <ProductConsumptionJournalEntry key={value[0].id} product={value[1]}
