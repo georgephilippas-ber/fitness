96922552901fd463c724c7935b69112cacd3a066
georgephilippas-ber <georgephilippas-ber@outlook.com>
2023-05-25 12:27:31 +0300

    Initial commit

--- mobile-ionic/src/App.tsx
+++ mobile-ionic/src/App.tsx
@@ -1,6 +1,7 @@
 import { IonButton, IonPage, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 
 /* Core CSS required for Ionic components to work properly */
+/* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
