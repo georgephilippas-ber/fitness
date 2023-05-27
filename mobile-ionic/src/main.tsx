62e7c4d723892aae65b39557a522605bb1985d4d
georgephilippas-ber <georgephilippas-ber@outlook.com>
2023-05-28 00:52:21 +0300

    Initial Commit

--- mobile-ionic/src/main.tsx
+++ mobile-ionic/src/main.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
 import axios from "axios";
+import {DateTime} from "luxon";
 
-axios.defaults.headers.common["authenticated-user"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjotMiwicm9sZSI6InVzZXIiLCJpYXQiOjE2ODUyMTc1NjAsImV4cCI6MTY4NTIyNDc2MH0.3FUFa_G1Sc49YWoyu1Jo7tVAkSnVBBA-e6M3Whn2N2A";
+axios.defaults.headers.common["authenticated-user"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjotMiwicm9sZSI6InVzZXIiLCJpYXQiOjE2ODUyMjM2NzksImV4cCI6MTY4NTIzMDg3OX0.0xq4SLPsQ9XwuJ3x4NVBy-itGlWwicPkxTtEdksXRGg";
 
-axios.post("http://localhost:4096/activities/")
+axios.post("http://localhost:4096/activity/latest").then(value => {
+    console.log(DateTime.fromMillis(value.data.referenceDate).toLocaleString(DateTime.DATE_HUGE));
+    console.log((value.data.referenceDate));
+    console.log((value.data));
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
