095655fab7c2e11264b3714b851f7f0685de114a
georgephilippas-ber <georgephilippas-ber@outlook.com>
2023-05-27 23:17:09 +0300

    Initial Commit

--- mobile-ionic/src/main.tsx
+++ mobile-ionic/src/main.tsx
@@ -5,6 +5,8 @@ import axios from "axios";
 
 axios.defaults.headers.common["authenticated-user"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjotMiwicm9sZSI6InVzZXIiLCJpYXQiOjE2ODUyMTc1NjAsImV4cCI6MTY4NTIyNDc2MH0.3FUFa_G1Sc49YWoyu1Jo7tVAkSnVBBA-e6M3Whn2N2A";
 
+axios.post("http://localhost:4096/activities/")
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
