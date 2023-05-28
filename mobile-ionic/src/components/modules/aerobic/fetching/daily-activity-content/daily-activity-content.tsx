b658757527d5d3179545eed591190ea61cdb3b56
georgephilippas-ber <georgephilippas-ber@outlook.com>
2023-05-28 19:30:00 +0300

    Initial Commit

--- mobile-ionic/src/components/modules/aerobic/fetching/daily-activity-content/daily-activity-content.tsx
+++ mobile-ionic/src/components/modules/aerobic/fetching/daily-activity-content/daily-activity-content.tsx
@@ -1,16 +1,23 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {DailyActivity} from "../../charts/daily-activity/daily-activity";
 import {fake_daily_activity} from "@shared/common/faker/activities";
 import {IonContent} from "@ionic/react";
+import axios from "axios";
+import {daily_activity_type} from "@shared/common/schema/activities/activities";
 
 export function DailyActivityContent() {
-    useEffect(() => {
+    const [daily_activity, set_daily_activity] = useState<daily_activity_type | undefined>(undefined);
 
+    useEffect(() => {
+        axios.post<undefined, daily_activity_type>("http://localhost:4096/activity/latest").then(value => {
+            set_daily_activity(daily_activity);
+        });
     }, []);
 
     return (
-        <IonContent style={{height: "80%"}}>
-            <DailyActivity daily_activity={fake_daily_activity()}/>
-        </IonContent>
+        daily_activity ?
+            <IonContent>
+                <DailyActivity daily_activity={daily_activity}/>
+            </IonContent> : null
     )
 }
