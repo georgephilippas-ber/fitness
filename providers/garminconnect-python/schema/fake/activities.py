b99b057b477400a0454be76dc34263473e22e196
georgephilippas-ber <georgephilippas-ber@outlook.com>
2023-05-30 12:53:32 +0300

    Initial Commit

--- providers/garminconnect-python/schema/fake/activities.py
+++ providers/garminconnect-python/schema/fake/activities.py
@@ -8,6 +8,7 @@ from mimesis import Numeric, Address
 from features.time_feature.time_feature import day, When
 
 from schema.activities import RunningInterface
+from typing import List
 
 numeric = Numeric()
 address = Address()
@@ -39,7 +40,7 @@ def running(user_id: int) -> RunningInterface:
         name="running",
         raw_name="",
         activeZoneMinutes=numeric.integer_number(start=4, end=16),
-        distance=numeric.integer_number(start=int(1.e3), end=int(3.e3)),
+        distance=numeric.integer_number(start=int(1.e3), end=int(2.e3)),
         maximumSpeed=0,
         averageHeartRate=numeric.integer_number(start=120, end=160),
         maximumHeartRate=numeric.integer_number(start=160, end=180),
@@ -48,9 +49,18 @@ def running(user_id: int) -> RunningInterface:
     )
 
 
-def running_activities(user_id: int, cardinality: int = 0x10):
+def running_activities(user_id: int, cardinality: int = 0x04):
     return [running(user_id) for index_ in range(0, cardinality)]
 
 
+def distance(running_activities_: List[RunningInterface]):
+    return list(map(lambda x: x.distance, running_activities_))
+
+
+def duration(running_activities_: List[RunningInterface]):
+    return list(map(lambda x: x.duration, running_activities_))
+
+
 if __name__ == "__main__":
-    print(running_activities(-2))
+    print(distance(running_activities(-2)))
+    print(duration(running_activities(-2)))
